23bc5631dcd34e8e82316fedc977cebb1a4374c6
podusit <d.rimdusit@gmail.com>
2024-11-01 13:54:29 +0700

    feat:context API to header and viewProductPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
+import { createContext } from "react";
+
+export const dataUser = createContext({});
 
 function App() {
   const userData = {
@@ -12,14 +14,16 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/view/:id" element={<ViewProductPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <dataUser.Provider value={userData}>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/view/:id" element={<ViewProductPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </dataUser.Provider>
   );
 }
 
--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -1,8 +1,11 @@
+import { useContext } from "react";
+import { dataUser } from "../App.jsx";
 function AppHeader() {
+  const { username } = useContext(dataUser);
   return (
     <div className="app-header-container">
       <h1 className="app-title">Products</h1>
-      <h2>สวัสดีคุณ (x)</h2>
+      <h2>สวัสดีคุณ {username}</h2>
     </div>
   );
 }
